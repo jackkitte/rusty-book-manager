e2be452914874060086f53ad8b1bd1fecb10a0bb
kakeru.tamashiro <kakeru.tamashiro@revcomm.co.jp>
2025-06-26 16:29:07 +0900

    settings adapter layer

---
+++ adapter/src/database/mod.rs
@@ -0,0 +1,27 @@
+use shared::config::DatabaseConfig;
+
+use sqlx::{PgPool, postgres::PgConnectOptions};
+
+fn make_pg_connect_options(config: &DatabaseConfig) -> PgConnectOptions {
+    PgConnectOptions::new()
+        .host(&config.host)
+        .port(config.port)
+        .username(&config.username)
+        .password(&config.password)
+        .database(&config.database)
+}
+
+#[derive(Clone)]
+pub struct ConnectionPool(PgPool);
+
+impl ConnectionPool {
+    pub fn inner_ref(&self) -> &PgPool {
+        &self.0
+    }
+}
+
+pub fn connect_database_with(config: &DatabaseConfig) -> ConnectionPool {
+    let options = make_pg_connect_options(config);
+    let pool = PgPool::connect_lazy_with(options);
+    ConnectionPool(pool)
+}
