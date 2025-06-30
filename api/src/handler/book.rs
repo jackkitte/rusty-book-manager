9a428e5311b718bd3ecd2f6b80477d558a5bcd7d
kakeru.tamashiro <kakeru.tamashiro@revcomm.co.jp>
2025-06-30 21:50:12 +0900

    add books api

---
+++ api/src/handler/book.rs
@@ -0,0 +1,66 @@
+use axum::{
+    Json,
+    extract::{Path, State},
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+use registry::AppRegistry;
+use thiserror::Error;
+use uuid::Uuid;
+
+use crate::model::book::{BookResponse, CreateBookRequest};
+
+#[derive(Error, Debug)]
+pub enum AppError {
+    #[error("{0}")]
+    InternalError(#[from] anyhow::Error),
+}
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, "").into_response()
+    }
+}
+
+pub async fn register_book(
+    State(registry): State<AppRegistry>,
+    Json(request): Json<CreateBookRequest>,
+) -> Result<StatusCode, AppError> {
+    registry
+        .book_repository()
+        .create(request.into())
+        .await
+        .map(|_| StatusCode::CREATED)
+        .map_err(AppError::from)
+}
+
+pub async fn show_book_list(
+    State(registry): State<AppRegistry>,
+) -> Result<Json<Vec<BookResponse>>, AppError> {
+    registry
+        .book_repository()
+        .find_all()
+        .await
+        .map(|books| {
+            books
+                .into_iter()
+                .map(BookResponse::from)
+                .collect::<Vec<_>>()
+        })
+        .map(Json)
+        .map_err(AppError::from)
+}
+
+pub async fn show_book(
+    State(registry): State<AppRegistry>,
+    Path(book_id): Path<Uuid>,
+) -> Result<Json<BookResponse>, AppError> {
+    registry
+        .book_repository()
+        .find_by_id(book_id)
+        .await
+        .and_then(|book| match book {
+            Some(book) => Ok(Json(book.into())),
+            None => Err(anyhow::anyhow!("The specific book was not found.")),
+        })
+        .map_err(AppError::from)
+}
--- api/src/route/book.rs
+++ api/src/route/book.rs
@@ -10,7 +10,7 @@ pub fn build_book_routers() -> Router<AppRegistry> {
     let books_routers = Router::new()
         .route("/", post(register_book))
         .route("/", get(show_book_list))
-        .route("/:book_id", get(show_book));
+        .route("/{book_id}", get(show_book));
 
     Router::new().nest("/books", books_routers)
 }
