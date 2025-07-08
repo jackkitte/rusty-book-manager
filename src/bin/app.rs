9c4d282384a95417d42a698f9a5118dea374f722
kakeru.tamashiro <kakeru.tamashiro@revcomm.co.jp>
2025-07-08 23:24:46 +0900

    remove unused package

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -2,7 +2,7 @@ use std::net::{Ipv4Addr, SocketAddr};
 
 use adapter::database::connect_database_with;
 use anyhow::Context;
-use anyhow::{Error, Result};
+use anyhow::Result;
 use api::route::{book::build_book_routers, health::build_health_check_routers};
 use axum::Router;
 use registry::AppRegistry;
