3976a19384085f9db05935e2956f58cdad91c437
kakeru.tamashiro <kakeru.tamashiro@revcomm.co.jp>
2025-07-15 07:32:03 +0900

    add auth config to registry

--- registry/src/lib.rs
+++ registry/src/lib.rs
@@ -1,24 +1,42 @@
 use std::sync::Arc;
 
-use adapter::repository::book::BookRepositoryImpl;
-use adapter::{database::ConnectionPool, repository::health::HealthCheckRepositoryImpl};
-use kernel::repository::book::BookRepository;
-use kernel::repository::health::HealthCheckRepository;
+use adapter::{
+    database::ConnectionPool,
+    redis::RedisClient,
+    repository::{
+        auth::AuthRepositoryImpl, book::BookRepositoryImpl, health::HealthCheckRepositoryImpl,
+    },
+};
+use kernel::repository::{
+    auth::AuthRepository, book::BookRepository, health::HealthCheckRepository,
+};
+use shared::config::AppConfig;
 
 #[derive(Clone)]
 pub struct AppRegistry {
     health_check_repository: Arc<dyn HealthCheckRepository>,
     book_repository: Arc<dyn BookRepository>,
+    auth_repository: Arc<dyn AuthRepository>,
 }
 
 impl AppRegistry {
-    pub fn new(connection_pool: ConnectionPool) -> Self {
+    pub fn new(
+        connection_pool: ConnectionPool,
+        redis_client: Arc<RedisClient>,
+        app_config: AppConfig,
+    ) -> Self {
         let health_check_repository =
             Arc::new(HealthCheckRepositoryImpl::new(connection_pool.clone()));
         let book_repository = Arc::new(BookRepositoryImpl::new(connection_pool.clone()));
+        let auth_repository = Arc::new(AuthRepositoryImpl::new(
+            connection_pool.clone(),
+            redis_client.clone(),
+            app_config.auth.ttl,
+        ));
         Self {
             health_check_repository,
             book_repository,
+            auth_repository,
         }
     }
 
@@ -27,5 +45,8 @@ impl AppRegistry {
     }
     pub fn book_repository(&self) -> Arc<dyn BookRepository> {
         self.book_repository.clone()
+    }
+    pub fn auth_repository(&self) -> Arc<dyn AuthRepository> {
+        self.auth_repository.clone()
     }
 }
